7806d70b7e1d64e68a116ada307723df7c2b3ea8
oppositecategory <christyaccount@hotmail.com>
2025-03-09 17:36:52 +0200

    updated repo

--- distributed_EM.py
+++ distributed_EM.py
@@ -12,18 +12,14 @@ import numpy as np
 import mrcfile
 from tqdm import tqdm
 
+import pickle
 from PIL import Image 
 
+from scipy.ndimage import gaussian_filter
 
 
 @torch.no_grad()
 def expectation_maximization(device,A, data, std, std_xy, K=10000,N=100):
-    """
-    TODO:
-    - Add scaling aswell including it into the prior
-    - Check importance sampling
-    - Check into ways to reduce sampling in later iterations
-    """
     n = A.shape[0]    
     batch_size = K // 10
     num_batch = K // batch_size
@@ -31,55 +27,73 @@ def expectation_maximization(device,A, data, std, std_xy, K=10000,N=100):
     normal_dist = MultivariateNormal(loc=torch.zeros(2).to(device), 
                                      covariance_matrix=std_xy * torch.eye(2).to(device))
     uniform_dist = Uniform(0,2*torch.pi)
-    beta_dist = Beta(2, 5)
+    beta_dist = Beta(torch.tensor(2.0).to(device), torch.tensor(5.0).to(device))
 
     A_t = torch.zeros_like(A,dtype=torch.float32).to(device)
-    data = data
-    std = std
+    std_t = torch.tensor(0,dtype=torch.float32).to(device)
+    stdxy_t = torch.tensor(0,dtype=torch.float32).to(device)
+    Z = torch.tensor(0, dtype=torch.float32).to(device)
     for i in tqdm(range(N)):
         X = data[i]
 
         integral = torch.zeros_like(A_t,dtype=torch.float32)
+        std_batch = torch.tensor(0,dtype=torch.float32).to(device) 
+        stdxy_batch = torch.tensor(0,dtype=torch.float32).to(device)
+        Z_batch = torch.tensor(0, dtype=torch.float32).to(device)
+
         for _ in range(num_batch):
             thetas = uniform_dist.sample((batch_size,)).to(device)
             translations = normal_dist.sample((batch_size,)).to(device)
-            scales = beta_dist.sample((batch_size,))
+            scales = beta_dist.sample((batch_size,)).to(device)
+            
+
+            thetas_logits = uniform_dist.log_prob(thetas).to(device)
+            translations_logits = normal_dist.log_prob(translations).to(device)
+            scales_logits = beta_dist.log_prob(scales/5.0).to(device) - torch.log(torch.tensor(5.0))
+
+            scales = 5.0*scales
 
             transformations = torch.zeros((batch_size, 2, 3)).to(device)
             transformations[:, 0, 0] = torch.cos(thetas)
             transformations[:, 0, 1] = -torch.sin(thetas)
             transformations[:, 1, 0] = torch.sin(thetas)
             transformations[:, 1, 1] = torch.cos(thetas)
-            transformations[:, :, 2] = 2*translations / n
+            transformations[:, :, 2] = 2*translations / n # PyTorch assumes translations are in [-1,1]
 
             grid = F.affine_grid(transformations, size=(batch_size, 1, n, n), align_corners=False)
-            #A_psi = F.grid_sample(A.expand(K, 1, n, n), grid, align_corners=False).squeeze(1) * scales.view(-1, 1, 1)
-            A_psi = F.grid_sample(A.expand(batch_size, 1, n, n), grid, align_corners=False).squeeze(1)
+            A_psi = F.grid_sample(A.expand(batch_size, 1, n, n), grid, align_corners=False).squeeze(1) * (scales.view(-1,1,1))
 
             inv_rotations = transformations[...,:2].permute(0,2,1)
             inv_t = -torch.bmm(inv_rotations,transformations[...,2:])
             inv_trans = torch.cat([inv_rotations,inv_t],dim=-1)
-            inv_grid = F.affine_grid(inv_trans,size=(batch_size,1,n,n), align_corners=False)
+            inv_grid = F.affine_grid(inv_trans,size=(batch_size,1,n,n), align_corners=False) 
 
-            #X_psi = F.grid_sample(X.expand(K, 1, n, n), inv_grid, align_corners=False).squeeze(1) * scales.view(-1, 1, 1)
-            X_psi = F.grid_sample(X.expand(batch_size, 1, n, n), inv_grid, align_corners=False).squeeze(1) 
+            X_psi = F.grid_sample(X.expand(batch_size, 1, n, n) , inv_grid, align_corners=False).squeeze(1) / scales.view(-1,1,1)
 
-            X_log_density = -torch.norm(X - A_psi,dim=(1,2)) ** 2 / (2*std ** 2)
-            X_log_density = X_log_density - torch.logsumexp(X_log_density,0) # Normalize logits
+            X_log_density = -(torch.norm(X - A_psi,dim=(1,2))) ** 2 / (2*std ** 2)
+            X_log_density = X_log_density - torch.logsumexp(X_log_density,0)
 
-            psi_log_density = -(torch.norm(translations, dim=1) / std_xy) ** 2 * 0.5 - 2*torch.log(2 * torch.pi * std_xy)
-  
+            psi_log_density = thetas_logits + translations_logits + scales_logits
+
             w = torch.exp(X_log_density + psi_log_density)
             w /= w.sum()
-            #w /= importance_sampling
-            #tqdm.write(f"W unique: {w.unique()}")
-            integral += ((w.view(batch_size, 1, 1) * X_psi)).sum(dim=0)
-            #del X_density, psi_density, A_psi, X_psi
+
+            Z_batch += (w * (scales**2)).sum()
+            std_batch += ( w.view(batch_size,1,1) * scales**2 * (torch.norm(X_psi - A,dim=(1,2))) ** 2 ).sum()
+            stdxy_batch += (w.view(batch_size,1,1) * (torch.norm(translations,dim=1)**2)).sum()
+            integral += (w.view(batch_size, 1, 1) * X_psi * (scales**2).view(batch_size,1,1)).sum(dim=0)
         A_t += integral/K
-    return A_t
+        std_t +=  std_batch / K
+        stdxy_t +=  stdxy_batch/K   
+        Z += Z_batch/K
+    std_t /= (n**2)
+    std_t /= data.shape[0]
+    std_t = std_t ** 0.5
+    stdxy_t /= 2*data.shape[0]
+    stdxy_t = stdxy_t ** 0.5
+    return A_t, std_t, stdxy_t,Z
 
-
-def distributed_EM(rank,world_size, A, data, std, std_xy,i):
+def distributed_EM(rank,world_size, A, data, std, std_xy, i):
     dist.init_process_group(backend="nccl", rank=rank, world_size=world_size)
     torch.cuda.set_device(rank)
     device = torch.device(f"cuda:{rank}")
@@ -96,52 +110,72 @@ def distributed_EM(rank,world_size, A, data, std, std_xy,i):
     dist.barrier()
 
     local_data = data.chunk(world_size)[rank].to(device)
-    local_result = expectation_maximization(device,A, local_data, std,std_xy)
+    A_local, std_local, stdxy_local,Z_local = expectation_maximization(device,A, local_data, std,std_xy, N = local_data.shape[0])
 
-    dist.all_reduce(local_result, op=dist.ReduceOp.SUM)
+    dist.all_reduce(A_local, op=dist.ReduceOp.SUM)
+    dist.all_reduce(std_local,op=dist.ReduceOp.SUM)
+    dist.all_reduce(stdxy_local,op=dist.ReduceOp.SUM)
+    dist.all_reduce(Z_local,op=dist.ReduceOp.SUM)
     dist.barrier()
 
     if rank == 0: 
-        A = local_result / data.shape[0]
-        A = (A - A.min())/ (A.max() - A.min())
-        A_np = A.cpu().numpy()*255
-        result = Image.fromarray(A_np.astype(np.uint8),'L')
-        result.save(f'experiment/{i}_iteration.png')
+        std = std_local 
+        stdxy = stdxy_local
+        n = A.shape[0]
+        A = A_local / Z_local
+        std /= (n**2)
+        std /= data.shape[0]
+        std = std ** 0.5
+        stdxy /= 2*data.shape[0]
+        stdxy = stdxy ** 0.5
+
+        statistics = np.array([std.cpu().numpy(),std_xy.cpu().numpy()])
+        with open(r"experiment/variance_estimatinon.obj","wb") as file:
+            pickle.dump(statistics,file)
+
+        A_np = A.cpu().numpy()
+        with mrcfile.new(f'experiment/{i}_iteration.mrc',overwrite=True) as mrc:
+            mrc.set_data(A_np)
     dist.destroy_process_group()
 
 
 
 if __name__ == "__main__":
-    #python -m torch.distributed.launch EM.py
+    #python -m torch.distributed.launch distributed_EM.py
     world_size = torch.cuda.device_count()
 
-    std = torch.tensor(2)
-    std_xy = torch.tensor(5)
+    std = torch.tensor(0.001)
+    std_xy = torch.tensor(20)
 
-    with mrcfile.open("data3.mrc",permissive=True) as mrc:
+    with mrcfile.open("data2.mrc",permissive=True) as mrc:
         data = mrc.data
 
-    init = np.array(Image.open('init.png').convert('L'))
-    #init = np.array(Image.open('experiment/3_iteration.png').convert('L'))
-    #init = np.mean(data,axis=0)
+    # img = np.array(Image.open("image.png").convert("L").resize((256,256)))
+    # img = img = (img - img.min())/(img.max() - img.min())
+    # init = gaussian_filter(img, sigma=5)      
+
+    with mrcfile.open("experiment/0_iteration.mrc",permissive=True) as mrc:
+        init = mrc.data 
+
     n = init.shape[0]
     A = torch.tensor(init,dtype=torch.float32).cuda()
     data = torch.tensor(data).type(torch.float32)
 
-    A = (A - A.min())/(A.max() - A.min())
-    data = (data - data.min())/(data.max() - data.min())
-
-    mp.spawn(distributed_EM, 
-             args=(world_size,A, data,std, std_xy,4), 
+    for i in range(1,2):
+        mp.spawn(distributed_EM, 
+             args=(world_size,A, data,std, std_xy,i), 
              nprocs=world_size, 
              join=True)
+        A = np.array(Image.open(f'experiment/{i}_iteration.mrc').convert('L'))
+        A = torch.tensor(A, dtype=torch.float32).cuda()
 
-    # for i in range(3):
-    #     mp.spawn(distributed_EM, 
-    #          args=(world_size,A, data,std, std_xy,4), 
-    #          nprocs=world_size, 
-    #          join=True)
-    #     A = np.array(Image.open(f'experiment/{i}_iteration.png').convert('L'))
+        with open(r"experiment/variance_estimatinon.obj","rb") as file:
+            deviations = pickle.load(file)
+        
+        std = torch.tensor(deviations[0],dtype=torch.float32).cuda()
+        std_xy = torch.tensor(deviations[1],dtype=torch.float32).cuda()
+        print("Updated noise std estimation:", deviations[0])
+        print("Updated translation std estimation:", deviations[1])
 
 
 
--- generate_data.py
+++ generate_data.py
@@ -5,19 +5,28 @@ from torch.distributions.multivariate_normal import MultivariateNormal
 from torch.distributions.beta import Beta
 
 import numpy as np
+
 import mrcfile
 
 from PIL import Image 
 
-def generate_data(K,std=20,std_xy=5,n=256):
-    img = Image.open("image.png").convert("L").resize((n,n))
+def generate_data(K,std=0.2,std_xy=5,n=256):
+    img = np.array(Image.open("image.png").convert("L").resize((n,n)))
     img = torch.tensor(np.array(img),dtype=torch.float32)
+    img = (img - img.min())/(img.max() - img.min())
+
+    # img = np.zeros((8,8))
+    # img[2:4,2:3] = 1
+    # img[5:7,2:3] = 1
+    # img[4:6,5:7] = 1
+    # img = torch.tensor(img,dtype=torch.float32)
 
     thetas = (2 * torch.pi) * torch.rand(size=(K,))
     normal_dist = MultivariateNormal(loc=torch.zeros(2), 
                                      covariance_matrix=std_xy * torch.eye(2))
     beta_dist = Beta(2, 5)
     translations = normal_dist.sample((K,))
+    scales = 5*beta_dist.sample((K,))
 
     transformations = torch.zeros((K, 2, 3))
     transformations[:, 0, 0] = torch.cos(thetas)
@@ -28,11 +37,10 @@ def generate_data(K,std=20,std_xy=5,n=256):
 
     grid = F.affine_grid(transformations, size=(K, 1, n, n), align_corners=False)
     data = F.grid_sample(img.expand(K, 1, n, n), grid, align_corners=False).squeeze(1)
-    #data = data.numpy()
-    data = data + torch.normal(mean=0,std=std,size=(K,n,n))
+    data = data * scales.view(K,1,1) + torch.normal(mean=0,std=std,size=(K,n,n))
 
     data = data.numpy()
-    with mrcfile.new('data3.mrc',overwrite=True) as mrc:
+    with mrcfile.new('data2.mrc',overwrite=True) as mrc:
         mrc.set_data(data)
 
 if __name__ == "__main__":
